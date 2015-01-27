46664bf92cdbcc814d33a894d995224db452fe45
Xinyi Zhang <xinyik14@yahoo.com>
2015-01-26 20:47:44 -0600

    generic with int type, throw error

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
-all: My_vec.o main.o
-	c++ -std=c++11 My_vec.o main.o -o main
-My_vec.o: My_vec.cpp My_vec.h
-	c++ -std=c++11 -c My_vec.cpp
-main.o: main.cpp My_vec.h
-	c++ -std=c++11 -c main.cpp
+all: main.o
+	c++ -std=c++11 main.o -o main
+main.o: main.cpp generic_My_vec.h
+	c++ -std=c++11 -c main.cpp 
 clean:
 	rm *.o main
--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 
 #include <iostream>
 #include <stdexcept>
-#include "My_vec.h"
+#include "generic_My_vec.h"
 
 int main() {
 	try {
+
+		cout<<"First try, type: char"<<endl;
 
 		My_vec<char> v;
 		// define an object v of My_vec type
@@ -36,7 +38,7 @@ int main() {
 		cout<<"Supposed to print: [C D E F G H I J K L M N O A B ]"<<endl;
 		cout << "size of v is: " << v.get_size() << endl;
 		cout<<"Supposed to print: 15"<<endl;
-
+		// insert 13 elements into the vector
 
 		v.insert_at_rank(10,'D');
 		// insert 'D' at the rank 10 into the vector v
@@ -101,10 +103,124 @@ int main() {
 		// test the function find_max_index using v2
 		sort_max(v2);
 		cout<<v2;
-		cout<<"Supposed to print: [A B C D G H I J K L D M N O Y]"<<endl;
+		cout<<"Supposed to print: [A B C D D G H I J K L M N O Y]"<<endl;
 		// test the function sort_max using v2
 
+		v2.replace_at_rank(14, 'S');
 		// replace in the vector v2 a character at the rank 14 with 'S'
+		
+
+
+
+
+
+
+
+
+
+
+
+
+	cout<<"Second try, type: int"<<endl;
+
+		My_vec<int> v_int;
+		// define an object v_int of My_vec type
+		v.insert_at_rank(0,2);
+		// insert 2 at the rank 0 into the vector v_int
+		cout << v_int;
+		cout<<"Supposed to print: [2 ]"<<endl;
+		// use the overloaded operator << to display vector elements
+		cout << "size of v_int is: " << v.get_size() << endl;
+		cout<<"Supposed to print: 1"<<endl;
+		// display the vector v_int size
+		v.insert_at_rank(0,1);
+		// insert 1 at the rank 0 into the vector v_int
+		cout << v_int;
+		cout<<"Supposed to print: [1 2 ]"<<endl;
+		// use the overloaded operator << to display vector elements
+		cout << "size of v_int is: " << v.get_size() << endl;
+		cout<<"Supposed to print: 2"<<endl;
+		// display the vector v_int size
+
+		int j = 0;
+		while (j < 13) {
+			v.insert_at_rank(i, 3 + i);
+			j++;
+		}
+		cout<<v_int;
+		cout<<"Supposed to print: [3 4 5 6 7 8 9 10 11 12 13 14 15 1 2 ]"<<endl;
+		cout << "size of v_int is: " << v.get_size() << endl;
+		cout<<"Supposed to print: 15"<<endl;
+		// insert 13 elements into the vector
+
+		v.insert_at_rank(10,4);
+		// insert int 4 at the rank 10 into the vector v_int
+		cout<<v_int;
+		cout<<"Supposed to print: [3 4 5 6 7 8 9 10 11 12 4 13 14 15 1 2 ]"<<endl;
+		// use the overloaded operator << to display vector elements
+		cout << "size of v_int is: " << v.get_size() << endl;
+		cout<<"Supposed to print: 16"<<endl;
+		// display the vector v_int size
+
+		v.remove_at_rank(2);
+		// remove a character at the rank 2 from the vector v_int
+		cout<<v_int;
+		cout<<"Supposed to print:[3 5 6 7 8 9 10 11 12 4 13 14 15 1 2 ]"<<endl;
+		// use the overloaded operator << to display vector elements
+		cout << "size of v_int is: " << v.get_size() << endl;
+		cout<<"Supposed to print: 15"<<endl;
+		// display the vector v_int size
+
+		v.replace_at_rank(2,5);
+		// replace a character at the rank 2 by the int 5
+		cout<<v_int;
+		cout<<"Supposed to print:[3 5 5 6 7 8 9 10 11 12 4 13 14 15 1 2]"<<endl;
+		// use the overloaded operator << to display vector elements
+		cout << "size of v_int is: " << v.get_size() << endl;
+		cout<<"Supposed to print: 15"<<endl;
+		// display the vector v_int size
+
+		My_vec<int> v_int_1(v_int);
+		// create a copy v_int_1 of the vector v_int using a copy constructor
+		cout<<v_int_1;
+		cout<<"Supposed to print:[3 5 5 6 7 8 9 10 11 12 4 13 14 15 1 2]"<<endl;
+		// use the overloaded operator << to display the vector v_int_1
+		v1.replace_at_rank(2,25);
+		// replace a character at the rank 2 of the vector v_int_1 with the int 25
+		cout<<v_int_1;
+		cout<<"Supposed to print:[3 25 5 6 7 8 9 10 11 12 4 13 14 15 1 2]"<<endl;
+		// use the overloaded operator << to display vector v_int_1 elements
+
+		My_vec<int> v_int_2;
+		// define an object v_int_2 of My_vec type
+		v2.insert_at_rank(0,11);
+		// insert 1 at the rank 0 into the vector v2
+		cout<<v_int_2;
+		cout<<"Supposed to print:[11 3 25 5 6 7 8 9 10 11 12 4 13 14 15 1 2 ]"<<endl;
+		// use the overloaded operator << to display vector elements
+		cout << "size of v_int_2 is: " << v_int_2.get_size() << endl;
+		cout<<"Supposed to print: 1"<<endl;
+		// display the vector v_int_2 size
+
+		v_int_2 = v_int_1;
+		// test the assignment operator and copy the vector v_int_1 to v_int_2
+		cout<<v_int_2;
+		cout<<"Supposed to print:[11 3 25 5 6 7 8 9 10 11 12 4 13 14 15 1 2]"<<endl;
+		// use the overloaded operator << to display vector v_int_2
+		cout << "size of v2 is: " << v2.get_size() << endl;
+		cout<<"Supposed to print: 15"<<endl;
+		// display the vector v_int_2 size
+
+		cout<<"Max index is"<<find_max_index(v_int_2, v_int_2.get_size())<<endl;
+		cout<<"Supposed to print: 2"<<endl;
+		// test the function find_max_index using v_int_2
+		sort_max(v_int_2);
+		cout<<v_int_2;
+		cout<<"Supposed to print: [1 2 3 4 5 6 7 8 9 10 11 11 12 13 14 15 25]"<<endl;
+		// test the function sort_max using v_int_2
+
+		v_int_2.replace_at_rank(14, 19);
+		// replace in the vector v_int_2 a character at the rank 14 with 19
 	}
 
 	catch (exception &error) {
